c122041a91326ef97d8d4f85ee8bfebb67aa04fc
vinaymavi <vinaymavi@gmail.com>
2024-05-25 16:57:13 -0400

    webapps and apis project `prettier` config sync.

--- src/types/video.types.ts
+++ src/types/video.types.ts
@@ -1,5 +1,4 @@
-import { IInput } from "@/src/types/types";
-import { type } from "doctrine";
+import { IInput } from '@/src/types/types';
 
 export interface IVideoStore {
   selectedLayoutId: string;
@@ -18,62 +17,62 @@ export interface IVideoStore {
 }
 
 export enum ELanguage {
-  "Afrikaans (South Africa)" = "af-ZA",
-  "Arabic" = "ar-XA",
-  "Basque (Spain)" = "eu-ES",
-  "Bengali (India)" = "bn-IN",
-  "Bulgarian (Bulgaria)" = "bg-BG",
-  "Catalan (Spain)" = "ca-ES",
-  "Chinese (Hong Kong)" = "yue-HK",
-  "Czech (Czech Republic)" = "cs-CZ",
-  "Danish (Denmark)" = "da-DK",
-  "Dutch (Belgium)" = "nl-BE",
-  "Dutch (Netherlands)" = "nl-NL",
-  "English (Australia)" = "en-AU",
-  "English (India)" = "en-IN",
-  "English (UK)" = "en-GB",
-  "English (US)" = "en-US",
-  "Filipino (Philippines)" = "fil-PH",
-  "Finnish (Finland)" = "fi-FI",
-  "French (Canada)" = "fr-CA",
-  "French (France)" = "fr-FR",
-  "German (Germany)" = "de-DE",
-  "Greek (Greece)" = "el-GR",
-  "Gujarati (India)" = "gu-IN",
-  "Hebrew (Israel)" = "he-IL",
-  "Hindi (India)" = "hi-IN",
-  "Hungarian (Hungary)" = "hu-HU",
-  "Icelandic (Iceland)" = "is-IS",
-  "Indonesian (Indonesia)" = "id-ID",
-  "Italian (Italy)" = "it-IT",
-  "Japanese (Japan)" = "ja-JP",
-  "Kannada (India)" = "kn-IN",
-  "Korean (South Korea)" = "ko-KR",
-  "Latvian (Latvia)" = "lv-LV",
-  "Lithuanian (Lithuania)" = "lt-LT",
-  "Malay (Malaysia)" = "ms-MY",
-  "Malayalam (India)" = "ml-IN",
-  "Mandarin Chinese (China)" = "cmn-CN",
-  "Mandarin Chinese (Taiwan)" = "cmn-TW",
-  "Marathi (India)" = "mr-IN",
-  "Norwegian (Norway)" = "nb-NO",
-  "Polish (Poland)" = "pl-PL",
-  "Portuguese (Brazil)" = "pt-BR",
-  "Portuguese (Portugal)" = "pt-PT",
-  "Punjabi (India)" = "pa-IN",
-  "Romanian (Romania)" = "ro-RO",
-  "Russian (Russia)" = "ru-RU",
-  "Serbian (Cyrillic)" = "sr-RS",
-  "Slovak (Slovakia)" = "sk-SK",
-  "Spanish (Spain)" = "es-ES",
-  "Spanish (US)" = "es-US",
-  "Swedish (Sweden)" = "sv-SE",
-  "Tamil (India)" = "ta-IN",
-  "Telugu (India)" = "te-IN",
-  "Thai (Thailand)" = "th-TH",
-  "Turkish (Turkey)" = "tr-TR",
-  "Ukrainian (Ukraine)" = "uk-UA",
-  "Vietnamese (Vietnam)" = "vi-VN",
+  'Afrikaans (South Africa)' = 'af-ZA',
+  'Arabic' = 'ar-XA',
+  'Basque (Spain)' = 'eu-ES',
+  'Bengali (India)' = 'bn-IN',
+  'Bulgarian (Bulgaria)' = 'bg-BG',
+  'Catalan (Spain)' = 'ca-ES',
+  'Chinese (Hong Kong)' = 'yue-HK',
+  'Czech (Czech Republic)' = 'cs-CZ',
+  'Danish (Denmark)' = 'da-DK',
+  'Dutch (Belgium)' = 'nl-BE',
+  'Dutch (Netherlands)' = 'nl-NL',
+  'English (Australia)' = 'en-AU',
+  'English (India)' = 'en-IN',
+  'English (UK)' = 'en-GB',
+  'English (US)' = 'en-US',
+  'Filipino (Philippines)' = 'fil-PH',
+  'Finnish (Finland)' = 'fi-FI',
+  'French (Canada)' = 'fr-CA',
+  'French (France)' = 'fr-FR',
+  'German (Germany)' = 'de-DE',
+  'Greek (Greece)' = 'el-GR',
+  'Gujarati (India)' = 'gu-IN',
+  'Hebrew (Israel)' = 'he-IL',
+  'Hindi (India)' = 'hi-IN',
+  'Hungarian (Hungary)' = 'hu-HU',
+  'Icelandic (Iceland)' = 'is-IS',
+  'Indonesian (Indonesia)' = 'id-ID',
+  'Italian (Italy)' = 'it-IT',
+  'Japanese (Japan)' = 'ja-JP',
+  'Kannada (India)' = 'kn-IN',
+  'Korean (South Korea)' = 'ko-KR',
+  'Latvian (Latvia)' = 'lv-LV',
+  'Lithuanian (Lithuania)' = 'lt-LT',
+  'Malay (Malaysia)' = 'ms-MY',
+  'Malayalam (India)' = 'ml-IN',
+  'Mandarin Chinese (China)' = 'cmn-CN',
+  'Mandarin Chinese (Taiwan)' = 'cmn-TW',
+  'Marathi (India)' = 'mr-IN',
+  'Norwegian (Norway)' = 'nb-NO',
+  'Polish (Poland)' = 'pl-PL',
+  'Portuguese (Brazil)' = 'pt-BR',
+  'Portuguese (Portugal)' = 'pt-PT',
+  'Punjabi (India)' = 'pa-IN',
+  'Romanian (Romania)' = 'ro-RO',
+  'Russian (Russia)' = 'ru-RU',
+  'Serbian (Cyrillic)' = 'sr-RS',
+  'Slovak (Slovakia)' = 'sk-SK',
+  'Spanish (Spain)' = 'es-ES',
+  'Spanish (US)' = 'es-US',
+  'Swedish (Sweden)' = 'sv-SE',
+  'Tamil (India)' = 'ta-IN',
+  'Telugu (India)' = 'te-IN',
+  'Thai (Thailand)' = 'th-TH',
+  'Turkish (Turkey)' = 'tr-TR',
+  'Ukrainian (Ukraine)' = 'uk-UA',
+  'Vietnamese (Vietnam)' = 'vi-VN',
 }
 
 export interface IProject {
@@ -91,7 +90,7 @@ export interface IMessage {
     url: string;
     text: string;
   };
-  intent: "success" | "error" | "warning";
+  intent: 'success' | 'error' | 'warning';
 }
 
 export interface IGeneratedVideoInfo {
@@ -112,9 +111,10 @@ export interface IVideo {
 }
 
 export enum EWorkerVersion {
-  V1 = "v1",
-  V2 = "v2",
+  V1 = 'v1',
+  V2 = 'v2',
 }
+
 export interface IGenerateVideoDto {
   url: string;
   version?: EWorkerVersion;
