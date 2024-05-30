456b0a2bc5ea0b02e09e1e6cb8a30764b0361dfa
vinaymavi <vinaymavi@gmail.com>
2024-05-30 14:41:11 -0400

    video.controller.ts copy a video with gemini translation.

--- src/helpers.ts
+++ src/helpers.ts
@@ -93,7 +93,6 @@ export function generatePreviewUrl(videoId: string) {
 }
 
 export const formatDate = (timestamp: number) => {
-  console.log('timestamp', timestamp);
   const date = new Date(timestamp * 1000);
   const day = date.getDate();
   const month = date.getMonth() + 1; // Months are zero based
