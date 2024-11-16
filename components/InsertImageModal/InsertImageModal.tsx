4b9719708e288d4c6aa6ee9f9ffede64a216cd5a
vinaymavi <vinaymavi@gmail.com>
2024-11-16 14:30:27 +0530

    Display project assets in image assets selection modal.

--- components/InsertImageModal/InsertImageModal.tsx
+++ components/InsertImageModal/InsertImageModal.tsx
@@ -2,7 +2,9 @@ import InsertImage, {
   IInsertImageProps,
 } from '@/components/InsertImage/InsertImage';
 import {
+  Button,
   Dialog,
+  DialogActions,
   DialogBody,
   DialogContent,
   DialogSurface,
@@ -13,6 +15,15 @@ import { DialogOpenChangeEventHandler } from '@fluentui/react-dialog';
 import { UploadStatus } from '@/src/types/common.types';
 import { useState } from 'react';
 import { DismissCircle24Filled } from '@fluentui/react-icons';
+import {
+  SelectTabData,
+  SelectTabEvent,
+  Tab,
+  TabList,
+} from '@fluentui/react-tabs';
+import { useVideoStore } from '@/src/stores/video.store';
+import { useQueryGetProject } from '@/src/query/video.query';
+import ProjectAssets from '../ProjectAssets/ProjectAssets';
 
 export function InsertImageModal({
   onUploadSuccess,
@@ -23,17 +34,43 @@ export function InsertImageModal({
   isDisplayURLUpload = true,
 }: IInsertImageModalProps) {
   const [uploadStatus, setUploadStatus] = useState<UploadStatus>('idle');
+  const [selectedTab, setSelectedTab] = useState<string>('desktop');
+  const [selectedAsset, setSelectedAsset] = useState<string | null>(null);
 
   const onUploadStatusChange = (status: UploadStatus) => {
     setUploadStatus(status);
   };
+  const projectId = useVideoStore((state) => state.currentProjectId);
+  const { data: projectData } = useQueryGetProject(projectId as string);
+  const TAB_DESKTOP = 'desktop';
+  const TAB_PROJECT = 'project';
+
+  const onTabSelect = (event: SelectTabEvent, data: SelectTabData) => {
+    setSelectedTab((data.value as string) || TAB_DESKTOP);
+  };
+
+  const onAssetSelect = (asset: string) => {
+    setSelectedAsset(asset);
+  };
+
   return (
     <Dialog open={isOpen} onOpenChange={onOpenChange}>
       <DialogSurface>
         <DialogBody>
           <DialogTitle>
             <div className={'flex justify-between'}>
-              <Subtitle1>{title ? title : 'Select Object'}</Subtitle1>
+              <TabList
+                size={'small'}
+                defaultSelectedValue={selectedTab}
+                onTabSelect={onTabSelect}
+              >
+                <Tab value={TAB_DESKTOP}>
+                  <Subtitle1>{title ? title : 'Select From Desktop'}</Subtitle1>
+                </Tab>
+                <Tab value={TAB_PROJECT}>
+                  <Subtitle1>Select Project Asset</Subtitle1>
+                </Tab>
+              </TabList>
               <div
                 className={
                   'relative -right-4 -top-4 cursor-pointer text-gray-600'
@@ -45,12 +82,36 @@ export function InsertImageModal({
             </div>
           </DialogTitle>
           <DialogContent className={'block'} style={{ height: '350px' }}>
-            <InsertImage
-              onUploadSuccess={onUploadSuccess}
-              onStateChange={onUploadStatusChange}
-              isDisplayURLUpload={isDisplayURLUpload}
-            />
+            {selectedTab === TAB_PROJECT ? (
+              <div>
+                <ProjectAssets
+                  assets={projectData?.assets || []}
+                  assetSize={'small'}
+                  onAssetSelect={onAssetSelect}
+                />
+              </div>
+            ) : (
+              <InsertImage
+                onUploadSuccess={onUploadSuccess}
+                onStateChange={onUploadStatusChange}
+                isDisplayURLUpload={isDisplayURLUpload}
+              />
+            )}
           </DialogContent>
+          <DialogActions>
+            {selectedTab === TAB_PROJECT && (
+              <Button
+                disabled={selectedAsset === null || selectedAsset === undefined}
+                appearance={'primary'}
+                onClick={() => {
+                  selectedAsset && onUploadSuccess(selectedAsset);
+                  onClose && onClose();
+                }}
+              >
+                Apply
+              </Button>
+            )}
+          </DialogActions>
         </DialogBody>
       </DialogSurface>
     </Dialog>
---
+++ components/ProjectAssets/index.ts
@@ -0,0 +1,1 @@
+export { ProjectAssets } from './ProjectAssets';
