9b5df8f399195745393985677d4a883f489e4703
vinaymavi <vinaymavi@gmail.com>
2025-03-18 22:02:36 +0530

    Refactor AuthLayout to remove deprecated queryClient initialization and streamline toast notification handling

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,35 +4,19 @@ import {
   FluentProvider,
   teamsLightTheme,
   Toaster,
-  useId,
 } from '@fluentui/react-components';
 import { useEffect } from 'react';
 import useSlidesStore from '../../src/stores/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import useMyToastController, {
-  TOAST_ID,
-} from '@/components/MyToast/MyToast.hook';
+import { TOAST_ID } from '@/components/MyToast/MyToast.hook';
 import { MyMessageBar } from '@/components/MyMessageBar';
+const queryClient = new QueryClient();
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   const { addUser, addServer } = useSlidesStore();
-  const { dispatchToast } = useMyToastController();
   // @Deprecated this initialization is deprecated will be removed in future
   // initialize local store
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      mutations: {
-        onError: (error) => {
-          dispatchToast({
-            title: 'Server Error',
-            body: 'Something went wrong :(',
-            intent: 'error',
-          });
-        },
-      },
-    },
-  });
   useEffect(() => {
     const user = document.querySelector("[data-store='addUser']")?.textContent;
     if (user && user?.length) {
